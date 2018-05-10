9057a4d4a5f2ea1dcebf399db0331a48a81dadf7
lincmba <lincolncmba@gmail.com>
2018-05-10 21:19:32 +0300

    Prediction using a given learning data and desired output

--- basics.py
+++ basics.py
@@ -8,7 +8,7 @@ classification:
 regression:
     boston house dataset
 """
-from sklearn import datasets
+from sklearn import datasets, svm
 
 
 def load_data():
@@ -18,6 +18,35 @@ def load_data():
     return iris, digits
 
 
+def predict_last(loaded_digits, last_predictions):
+    """
+    Learn from the training data except the last the use the last to test
+    the prediction
+    Using the digits datasets, predict given an image,
+    which digit it represents
+    Image is a 8 * 8 matrix
+
+    sample results 0 - 9 == digits.target
+
+    Use an estimator [uses fix(x, y) and predict(T)] to classify unseen samples
+    
+    :return: predicted value
+    """
+    # SVC implements Support Vector Classification
+    # gamma and C are parameters of the model and in this case manually set
+    classifier = svm.SVC(gamma=0.001, C=100.)  # The estimator
+
+    # Next, this estimator should be fitted/learn to/from the model
+    # Let's use the whole dataset as a training data except the last which
+    # we'll predict
+    classifier.fit(loaded_digits.data[:-1], loaded_digits.target[:-1])
+    # predict target of last data
+    predicted_values = classifier.predict(loaded_digits.data[-last_predictions:])
+    actual_values = loaded_digits.target[-last_predictions:]
+    return predicted_values, actual_values
+
+
 if __name__ == "__main__":
-    a, b = load_data()
-    print(a)
+    iris, digits = load_data()
+    predicted, actual = predict_last(digits, 10)
+    print(predicted, actual)
